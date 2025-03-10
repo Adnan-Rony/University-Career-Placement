8e1784ad155b9da5b323263a106a0e9cacc7d104
Adnan-Rony <adnanrony19@gmail.com>
2025-03-10 23:45:15 +0600

    user login register controller done

--- Backend/src/routers/index.routes.js
+++ Backend/src/routers/index.routes.js
@@ -3,19 +3,13 @@ import express from "express";
 const router = express.Router();
 
 // Define a GET route for "/"
-router.get("/", (req, res) => {
+router.get("/api/v1/", (req, res) => {
     return res.status(200).json({
         message: "Connected to the server",
         success: true
     });
 });
 
-// Define another example route
-router.get("/hello", (req, res) => {
-    return res.status(200).json({
-        message: "Hello from Express!",
-        success: true
-    });
-});
+
 
 export default router;
