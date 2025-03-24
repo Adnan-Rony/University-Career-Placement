9429e7079b6514f2dc3eca9150e46b962955c94d
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-03-24 15:19:55 +0600

    featured section added & initialized tanstack query

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,19 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { Toaster } from 'react-hot-toast'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router";
-import { AppRouter } from './router/AppRouter.jsx';
-import { RouterProvider } from 'react-router-dom';
+import { AppRouter } from "./router/AppRouter.jsx";
+import { RouterProvider } from "react-router-dom";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
-createRoot(document.getElementById('root')).render(
+
+// Create a client
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-   
-   
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={AppRouter}></RouterProvider>
-      
-  
 
-  <Toaster></Toaster>
-  </StrictMode>,
-)
+      <Toaster></Toaster>
+    </QueryClientProvider>
+  </StrictMode>
+);
