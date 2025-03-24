c3d6832c0044ca6ab550da809335a6ebf7d7e626
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-03-24 21:56:12 +0600

    initialized react hook form

---
+++ Frontend/src/Pages/Empoloyer/PostJob.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SectionTitle } from '../../Components/Shared/SectionTitle'
+
+export const PostJob = () => {
+  return (
+    <div>
+        <SectionTitle title={'Post Job'}></SectionTitle>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
