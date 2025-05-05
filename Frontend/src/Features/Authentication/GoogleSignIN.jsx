8e6d940ff69719577c7671af2bf37dd1c9c85145
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-05-05 23:52:56 +0600

    issue fixed

--- Frontend/src/Features/Authentication/GoogleSignIN.jsx
+++ Frontend/src/Features/Authentication/GoogleSignIN.jsx
@@ -1,58 +1,53 @@
-import React, { useContext } from 'react'
-import { Authcontext } from '../../Context/Authprovider'
-import { FaGoogle } from 'react-icons/fa'
-import toast from 'react-hot-toast'
-import { useNavigate } from 'react-router'
-import axios from 'axios'
+import React, { useContext } from "react";
+import { Authcontext } from "../../Context/Authprovider";
+import { FaGoogle } from "react-icons/fa";
+import { USER_API_END_POINT } from "../../utils/Constant";
+import { useNavigate } from "react-router";
+import axios from "axios";
 
 export const GoogleSignIN = () => {
-    const navigate = useNavigate();
-      const {user,logout,googleSignIn}=useContext(Authcontext)
-      // console.log(user);
-      const handleGoogleSignin=()=>{
-        googleSignIn()
-        .then((result) => {
-          
-          console.log(result.user);
-          const userInfo = {
-            fullname: result.user?.displayName,
-            email: result.user?.email,
-            profilePhoto: result.user?.photoURL,
+  const navigate = useNavigate();
+  const { user, logout, googleSignIn } = useContext(Authcontext);
+  // console.log(user);
+  const handleGoogleSignin = () => {
+    googleSignIn()
+      .then((result) => {
+        console.log(result.user);
+        const userInfo = {
+          fullname: result.user?.displayName,
+          email: result.user?.email,
+          profilePhoto: result.user?.photoURL,
+        };
 
-            
-          };
-          
-            // Send user data to backend for registration or login
-            axios.post(`${USER_API_END_POINT}/register`,userInfo)
-            .then(response=>{
-              console.log(response.data); 
-              console.log("UserInfo Sent",userInfo);
-              navigate("/"); 
-            })
-            .catch(error=>{
-              console.error("Error while storing user info:", error.response || error);
-            })
-
-           
-
-          
-            
-          }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-       
-        
+        // Send user data to backend for registration or login
+        axios
+          .post(`${USER_API_END_POINT}/register`, userInfo)
+          .then((response) => {
+            console.log(response.data);
+            console.log("UserInfo Sent", userInfo);
+            navigate("/");
+          })
+          .catch((error) => {
+            console.error(
+              "Error while storing user info:",
+              error.response || error
+            );
           });
-
-      }
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
   return (
     <div>
-        <button
+      <button
         onClick={handleGoogleSignin}
-        className="p-3 shadow-2xl rounded-lg hover:bg-gray-100 transition-all">
-                        <FaGoogle className="text-red-500" size={20} />
-                      </button>
+        className="p-3 shadow-2xl rounded-lg hover:bg-gray-100 transition-all"
+      >
+        <FaGoogle className="text-red-500" size={20} />
+      </button>
     </div>
-  )
-}
+  );
+};
