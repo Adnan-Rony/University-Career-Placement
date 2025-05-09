b0afbebd0f966aa5315bfb1f45684462b887268e
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-05-09 14:51:30 +0600

    added blog section

--- Frontend/src/Components/Home/Blog.jsx
+++ Frontend/src/Components/Home/Blog.jsx
@@ -1,9 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import React, { useEffect } from 'react'
+import { Link } from 'react-router'
+import { SectionTitle } from '../Shared/SectionTitle'
 
 export const Blog = () => {
-    const {error,data:blogs=[]}=useQuery({
+    const {error,data:AllBlogs=[]}=useQuery({
         queryKey:['blogs'],
         queryFn:async()=>{
             const res=await axios.get('/blog.json')
@@ -12,7 +14,7 @@ export const Blog = () => {
 
     })
 
-
+const blogs=AllBlogs.slice(0,3)
    const {author, comments,title,details,
 excerpt,id,date
 
@@ -21,8 +23,38 @@ excerpt,id,date
 
     
   return (
-    <div className='container mx-auto '>Blog
+   <div className='bg-r-background'>
+     <div className='container mx-auto py-16'>
+        <SectionTitle
+         title={"Latest Career Insights"}
+         subtitle={"Stay Informed with the Latest Career Advice and Job Trends"}/>
+
+    <div className='grid md:grid-cols-3 md:gap-8'>
+        {
+            blogs.map((blog)=><div key={blog.id}
+             className="card bg-base-100  shadow-md rounded-3xl ">
+  <figure className="px-4 py-4">
+    <img
+      src={blog.thumbnail}
+      alt="Shoes"
+      className="rounded-xl" />
+  </figure>
+  <div className="card-body  text-start">
+    <h2 className="card-title text-start">{blog.title}</h2>
+    <p className='line-clamp-2'>{blog.excerpt}</p>
+    <div className="card-actions">
+    
+      <Link>
+      <button className="btn btn-ghost btn-outline text-r-accent">Read More</button>
+      </Link>
+
+    </div>
+  </div>
+</div>)
+        }
+    </div>
     
     </div>
+   </div>
   )
 }
