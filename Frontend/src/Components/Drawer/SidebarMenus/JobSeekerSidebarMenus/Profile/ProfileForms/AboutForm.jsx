e89547d929624005ec5581f296c5e2693ebede96
Adnan-Rony <adnanrony19@gmail.com>
2025-08-04 22:11:58 +0600

    login page design

--- Frontend/src/Components/Drawer/SidebarMenus/JobSeekerSidebarMenus/Profile/ProfileForms/AboutForm.jsx
+++ Frontend/src/Components/Drawer/SidebarMenus/JobSeekerSidebarMenus/Profile/ProfileForms/AboutForm.jsx
@@ -1,27 +1,30 @@
-import { useContext, useRef, useState } from 'react';
-import { useForm, useFieldArray } from 'react-hook-form';
-import { ProfileContext } from '../../../../../../Context/ProfileProvider';
+import { useContext, useRef, useState } from "react";
+import { useForm, useFieldArray } from "react-hook-form";
+import { ProfileContext } from "../../../../../../Context/ProfileProvider";
 
-import toast from 'react-hot-toast';
-import { useUpdateProfile } from '../../../../../../hooks/useAuth';
+import toast from "react-hot-toast";
+import { useUpdateProfile } from "../../../../../../hooks/useAuth";
 
 export const AboutForm = () => {
   // Calling ProvderContext:Context APi
-  const { profileData, updateProfileSection } = useContext(ProfileContext)
-  const { mutate,isPending,isSuccess,isError   }=useUpdateProfile()
+  const { profileData, updateProfileSection } = useContext(ProfileContext);
+  const { mutate, isPending, isSuccess, isError } = useUpdateProfile();
   const fileInputRef = useRef();
   const [cover, setCover] = useState("");
-  const [isUploading,setIsUploading]=useState(false)
-  const { register, control, handleSubmit, watch, formState: { errors } } = useForm(
+  const [isUploading, setIsUploading] = useState(false);
+  const {
+    register,
+    control,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
-);
-
-
-//  Image Upload
+  //  Image Upload
   const handleCoverUpload = async (e) => {
     const file = e.target.files[0];
     if (!file) return;
-     setIsUploading(true); 
+    setIsUploading(true);
     const formData = new FormData();
     formData.append("file", file);
     formData.append("upload_preset", "blogging");
@@ -41,7 +44,7 @@ export const AboutForm = () => {
     } catch (error) {
       console.error("Upload failed:", error);
       toast.error("Failed to upload photo");
-    }finally{
+    } finally {
       setIsUploading(false);
     }
   };
@@ -49,40 +52,46 @@ export const AboutForm = () => {
   // Handle Submit
 
   const onSubmit = (data) => {
-    updateProfileSection('about', data);
+    updateProfileSection("about", data);
     const payload = {
       name: data.name,
       location: data.location,
       primaryRole: data.primaryRole,
       yearsExperience: data.yearsExperience,
       bio: data.bio,
-      picture: cover ,
+      picture: cover,
     };
     console.log(payload);
-    mutate(payload)
+    mutate(payload);
   };
- 
+
   return (
     <div>
       <div className="p-6 flex flex-col md:flex-row">
         <div className=" md:w-2/6">
-          <h1 className='text-2xl font-bold'>About</h1>
+          <h1 className="text-2xl font-bold">About</h1>
           <p className="text-gray-500 mb-4">
             Tell us about yourself so startups know who you are.
           </p>
         </div>
-        <form onSubmit={handleSubmit(onSubmit)}
+        <form
+          onSubmit={handleSubmit(onSubmit)}
           className="grid grid-cols-1 gap-6  md:px-8
-         w-full">
+         w-full"
+        >
           {/* Name */}
-          <div >
-            <label className="block text-sm font-medium text-gray-700">Your name*</label>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Your name*
+            </label>
             <input
-              {...register('name', { required: "Name is required" })}
+              {...register("name", { required: "Name is required" })}
               className="mt-1 w-full 
             input"
             />
-            {errors.name && <p className="text-red-500 text-sm mt-1">{errors.name.message}</p>}
+            {errors.name && (
+              <p className="text-red-500 text-sm mt-1">{errors.name.message}</p>
+            )}
           </div>
           {/* Image */}
           {/* <div className="flex items-center">
@@ -92,10 +101,10 @@ export const AboutForm = () => {
             <button type="button"
               className="btn btn-outline">Upload a new photo</button>
           </div> */}
-           {/* Profile Image Upload */}
+          {/* Profile Image Upload */}
           <div className="flex items-center">
             <img
-              src={ cover || "/defavatar.png"}
+              src={cover || "/defavatar.png"}
               alt="Profile"
               className="w-20 h-20 rounded-full mr-4  animate-pulse  border-gray-400 object-cover"
             />
@@ -117,34 +126,50 @@ export const AboutForm = () => {
           {/* ........ */}
           {/* Location */}
           <div className="">
-            <label className="block text-sm font-medium text-gray-700">Where are you based?*</label>
+            <label className="block text-sm font-medium text-gray-700">
+              Where are you based?*
+            </label>
             <input
-              {...register('location', { required: "Location is required" })}
+              {...register("location", { required: "Location is required" })}
               className="mt-1 input w-full"
             />
-            {errors.location && <p className="text-red-500 text-sm mt-1">{errors.location.message}</p>}
+            {errors.location && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.location.message}
+              </p>
+            )}
           </div>
           {/* Role */}
           <div>
-            <label className="block text-sm font-medium text-gray-700">Select your primary role*</label>
+            <label className="block text-sm font-medium text-gray-700">
+              Select your primary role*
+            </label>
             <select
-              {...register('primaryRole', { required: "Role is required" })}
+              {...register("primaryRole", { required: "Role is required" })}
               className="mt-1  w-full 
              select"
             >
-               <option value="">Select Select Role</option>
+              <option value="">Select Select Role</option>
               <option>Frontend Engineer</option>
               <option>Full-Stack Engineer</option>
               <option>Select role</option>
             </select>
-            {errors.primaryRole && <p className="text-red-500 text-sm mt-1">{errors.primaryRole.message}</p>}
+            {errors.primaryRole && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.primaryRole.message}
+              </p>
+            )}
           </div>
           {/* Experience */}
 
           <div>
-            <label className="block text-sm font-medium text-gray-700">Years of experience*</label>
+            <label className="block text-sm font-medium text-gray-700">
+              Years of experience*
+            </label>
             <select
-              {...register('yearsExperience', { required: "Experience is required" })}
+              {...register("yearsExperience", {
+                required: "Experience is required",
+              })}
               className="mt-1 input  w-full"
             >
               <option value="">Select experience</option>
@@ -152,35 +177,39 @@ export const AboutForm = () => {
               <option>1 Year</option>
               <option>2 Years</option>
             </select>
-            {errors.yearsExperience && <p className="text-red-500 text-sm mt-1">{errors.yearsExperience.message}</p>}
+            {errors.yearsExperience && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.yearsExperience.message}
+              </p>
+            )}
           </div>
           {/* Bio */}
           <div className="">
-            <label className="block text-sm font-medium text-gray-700">Your bio</label>
+            <label className="block text-sm font-medium text-gray-700">
+              Your bio
+            </label>
             <textarea
-              {...register('bio', {
-                required: "Bio is required",
+              {...register("bio", {
+                // required: "Bio is required",
                 minLength: {
                   value: 20,
-                  message: "Bio must be at least 20 characters"
-                }
+                  message: "Bio must be at least 20 characters",
+                },
               })}
               className="mt-1 textarea  w-full"
               rows="4"
             />
-            {errors.bio && <p className="text-red-500 text-sm mt-1">{errors.bio.message}</p>}
+            {errors.bio && (
+              <p className="text-red-500 text-sm mt-1">{errors.bio.message}</p>
+            )}
           </div>
           <div>
-            <button 
-            type='submit'
-            className="btn">
+            <button type="submit" className="btn">
               Submit
             </button>
           </div>
-
         </form>
-
       </div>
     </div>
-  )
-}
+  );
+};
